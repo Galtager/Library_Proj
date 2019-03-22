3ac61888e52a6a7c40015c44c7a3a3a36cf91dbe
Lidor <lidorhadjaj@gmail.com>
2019-03-22 10:13:04 +0200

    Some minor changes - testing the repo

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ class Main
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-    	int i =0;
-    	i++;
         System.out.println("Hello, World"); 
     } 
-} 
+}
