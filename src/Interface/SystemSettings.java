b9e1ae636c0c4ade887b29237556a637a57790f6
lidor <lidorhadjaj@gmail.com>
2019-07-11 12:19:57 +0300

    More features

--- src/Interface/SystemSettings.java
+++ src/Interface/SystemSettings.java
@@ -19,17 +19,18 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.text.NumberFormat;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.NumberFormatter;
+
 import java.awt.Window.Type;
 
 public class SystemSettings {
 
 	JFrame frameClass;
-	private JTextField textField_backup;
-	private JTextField textField_DataSize;
 	private JTextField JTextFiel_Path;
 	private JLabel jLable1;
 	byte[] image;
@@ -84,29 +85,25 @@ public class SystemSettings {
 		Tab_SystemOptions.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		tabbedPane.addTab("System Options", null, Tab_SystemOptions, null);
 		
-		JCheckBox jcb_2 = new JCheckBox("Enable automatic numbering for student code in addition");
-		jcb_2.setBounds(213, 129, 539, 33);
-		jcb_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
-		JCheckBox jcb_3 = new JCheckBox("Enable automatic numbering for title code in addition");
-		jcb_3.setBounds(213, 201, 507, 33);
-		jcb_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
-		JLabel JLable_LoanDaysValue = new JLabel("- Default value for loan days Title");
-		JLable_LoanDaysValue.setBounds(78, 269, 315, 25);
+		JLabel JLable_LoanDaysValue = new JLabel("Max days for loan");
+		JLable_LoanDaysValue.setBounds(61, 32, 171, 25);
 		JLable_LoanDaysValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		
-		JLabel JLable_PaymentValue = new JLabel("- Default payment value");
-		JLable_PaymentValue.setBounds(78, 321, 225, 25);
-		JLable_PaymentValue.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
-		JFormattedTextField jtf_LoanDaysValue = new JFormattedTextField();
-		jtf_LoanDaysValue.setBounds(395, 264, 41, 39);
+		NumberFormat format = NumberFormat.getInstance();
+	    NumberFormatter formatter = new NumberFormatter(format);
+	    formatter.setValueClass(Integer.class);
+	    formatter.setMinimum(0);
+	    formatter.setMaximum(Integer.MAX_VALUE);
+	    formatter.setAllowsInvalid(false);
+		JFormattedTextField jtf_LoanDaysValue = new JFormattedTextField(formatter);
+		jtf_LoanDaysValue.setText(String.valueOf(Globals.days_per_borrow));
+		jtf_LoanDaysValue.setBounds(242, 29, 176, 39);
 		
 		JButton jb_Save1 = new JButton("Save");
 		jb_Save1.setBounds(786, 365, 75, 33);
 		jb_Save1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				Globals.days_per_borrow = Integer.parseInt(jtf_LoanDaysValue.getText());
 			}
 		});
 		jb_Save1.setFont(new Font("Tahoma", Font.PLAIN, 18));
@@ -120,82 +117,10 @@ public class SystemSettings {
                 }
         });
 		Tab_SystemOptions.setLayout(null);
-		
-		JCheckBox jcb_1 = new JCheckBox("Canceling a delay message Returns a title");
-		jcb_1.setBounds(213, 53, 407, 33);
-		jcb_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		Tab_SystemOptions.add(jcb_1);
-		Tab_SystemOptions.add(jcb_2);
-		Tab_SystemOptions.add(jcb_3);
 		Tab_SystemOptions.add(JLable_LoanDaysValue);
-		Tab_SystemOptions.add(JLable_PaymentValue);
 		Tab_SystemOptions.add(jtf_LoanDaysValue);
-		
-		JFormattedTextField jtf_PaymentValue = new JFormattedTextField();
-		jtf_PaymentValue.setBounds(302, 310, 41, 39);
-		Tab_SystemOptions.add(jtf_PaymentValue);
 		Tab_SystemOptions.add(jb_Close1);
 		Tab_SystemOptions.add(jb_Save1);
-		
-		JPanel Tab_Backup = new JPanel();
-		Tab_Backup.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		tabbedPane.addTab("Backup", null, Tab_Backup, null);
-		Tab_Backup.setLayout(null);
-		
-		JLabel jl_lDataSize = new JLabel("Database size:");
-		jl_lDataSize.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		jl_lDataSize.setBounds(166, 58, 118, 32);
-		Tab_Backup.add(jl_lDataSize);
-		
-		JButton btnFolderBackup = new JButton("Folder backup");
-		btnFolderBackup.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnFolderBackup.setBounds(162, 116, 262, 84);
-		Tab_Backup.add(btnFolderBackup);
-		
-		JButton btnBackupAndCompression = new JButton("Backup and compression");
-		btnBackupAndCompression.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnBackupAndCompression.setBounds(457, 116, 262, 84);
-		Tab_Backup.add(btnBackupAndCompression);
-		
-		JButton jb_Close2 = new JButton("Close");
-		jb_Close2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		jb_Close2.setBounds(674, 365, 93, 33);
-		jb_Close2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent e) {
-                frameClass.dispose();
-                }
-        });
-		Tab_Backup.add(jb_Close2);
-		
-		JButton jb_Save2 = new JButton("Save");
-		jb_Save2.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		jb_Save2.setBounds(776, 365, 85, 33);
-		Tab_Backup.add(jb_Save2);
-		
-		JLabel lbl_recommend = new JLabel("It is recommended to perform once a week compression");
-		lbl_recommend.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lbl_recommend.setBounds(166, 216, 553, 20);
-		Tab_Backup.add(lbl_recommend);
-		
-		JLabel lbl_backup = new JLabel("Permanent location to backup the database");
-		lbl_backup.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lbl_backup.setBounds(162, 252, 553, 20);
-		Tab_Backup.add(lbl_backup);
-		
-		textField_backup = new JTextField();
-		textField_backup.setBounds(162, 280, 469, 26);
-		Tab_Backup.add(textField_backup);
-		textField_backup.setColumns(10);
-		
-		JButton btn_Delete1 = new JButton("Delete");
-		btn_Delete1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btn_Delete1.setBounds(634, 280, 106, 27);
-		Tab_Backup.add(btn_Delete1);
-		
-		textField_DataSize = new JTextField();
-		textField_DataSize.setBounds(283, 62, 436, 26);
-		Tab_Backup.add(textField_DataSize);
-		textField_DataSize.setColumns(10);
 		
 		JPanel Tab_DeleteData = new JPanel();
 		Tab_DeleteData.setFont(new Font("Tahoma", Font.PLAIN, 20));
